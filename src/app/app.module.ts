621a27f37a76ea68cb67bd7d5181d5131cfcc55a
Chiranjib Das <chiranjibd47@gmail.com>
2024-11-24 12:02:14 +0530

    Navbar and modes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CircularAnimationComponent } from './components/circular-animation/circular-animation.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CircularAnimationComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Output, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {
+  isDarkMode = false;
+  @Output() modeEmitter = new EventEmitter<boolean>();
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    // Optional: Save preference to localStorage
+    localStorage.setItem('darkMode', JSON.stringify(this.isDarkMode));
+    // Optional: Apply dark mode to entire body
+    if (this.isDarkMode) {
+      document.body.classList.add('bg-dark', 'text-light');
+      this.modeEmitter.emit(true);
+    } else {
+      document.body.classList.remove('bg-dark', 'text-light');
+      this.modeEmitter.emit(false);
+    }
+  }
+
+  ngOnInit() {
+    // Optional: Load saved preference from localStorage
+    const savedMode = localStorage.getItem('darkMode');
+    if (savedMode) {
+      this.isDarkMode = JSON.parse(savedMode);
+      if (this.isDarkMode) {
+        document.body.classList.add('bg-dark', 'text-light');
+      }
+    }
+  }
+}
