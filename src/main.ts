247e2eb59411b93f39dd3a1227a047e110fb9720
Chiranjib Das <chiranjibd47@gmail.com>
2024-11-25 16:37:40 +0530

    speed insights added vercel

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { injectSpeedInsights } from '@vercel/speed-insights/*';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+injectSpeedInsights();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
