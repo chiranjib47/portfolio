3aaf5cb4b57581565cf45f9746f42b9286664881
Chiranjib Das <chiranjibd47@gmail.com>
2024-11-30 18:21:20 +0530

    updated  the skills

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -22,5 +22,95 @@ export class SkillsComponent {
       name: 'React',
       backShadow: 'back-shadow-react',
     },
+
+    {
+      iconPath: 'assets/Icons/html.svg',
+      name: 'HTML',
+      backShadow: 'back-shadow-html',
+    },
+
+    {
+      iconPath: 'assets/Icons/css.svg',
+      name: 'CSS',
+      backShadow: 'back-shadow-css',
+    },
+
+    {
+      iconPath: 'assets/Icons/js.svg',
+      name: 'JavaScript',
+      backShadow: 'back-shadow-js',
+    },
+
+    {
+      iconPath: 'assets/Icons/ts.svg',
+      name: 'TypeScript',
+      backShadow: 'back-shadow-ts',
+    },
+
+    {
+      iconPath: 'assets/Icons/redux.svg',
+      name: 'Redux',
+      backShadow: 'back-shadow-redux',
+    },
+
+    {
+      iconPath: 'assets/Icons/java.svg',
+      name: 'Java',
+      backShadow: 'back-shadow-java',
+    },
+
+    {
+      iconPath: 'assets/Icons/nodejs.svg',
+      name: 'NodeJS',
+      backShadow: 'back-shadow-nodejs',
+    },
+
+    {
+      iconPath: 'assets/Icons/mysql.svg',
+      name: 'MySQL',
+      backShadow: 'back-shadow-mysql',
+    },
+
+    {
+      iconPath: 'assets/Icons/git.svg',
+      name: 'Git',
+      backShadow: 'back-shadow-git',
+    },
+
+    {
+      iconPath: 'assets/Icons/jenkins.svg',
+      name: 'Jenkins',
+      backShadow: 'back-shadow-jenkins',
+    },
+
+    {
+      iconPath: 'assets/Icons/jira.svg',
+      name: 'Jira',
+      backShadow: 'back-shadow-jira',
+    },
+
+    {
+      iconPath: 'assets/Icons/notion.svg',
+      name: 'Notion',
+      backShadow: 'back-shadow-notion',
+    },
+
+    {
+      iconPath: 'assets/Icons/docker.svg',
+      name: 'Docker',
+      backShadow: 'back-shadow-docker',
+    },
+
+    {
+      iconPath: 'assets/Icons/aws.svg',
+      name: 'AWS',
+      backShadow: 'back-shadow-aws',
+    },
+
+    {
+      iconPath: 'assets/Icons/azure.svg',
+      name: 'Azure',
+      backShadow: 'back-shadow-azure',
+    },
   ];
 }
